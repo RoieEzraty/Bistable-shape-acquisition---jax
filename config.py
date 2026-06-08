99bfa341335ca50158aa21c4d16d1891fc8b2b2f
SMR_Admin <hadar.strauss@mail.huji.ac.il>
2026-06-08 16:53:17 +0300

    before going to Germany, trying to make robot Update agree

--- config.py
+++ config.py
@@ -72,10 +72,10 @@ class VariablesConfig:
             # object.__setattr__(self, "thresh", 1.24)  # Mar12 dl90
             # object.__setattr__(self, "tau_file", "single_hinge_files/May22_old_dl90_toughend.csv")
             # object.__setattr__(self, "thresh", 1.1)  # May22_old_dl90_toughend
-            object.__setattr__(self, "tau_file", "single_hinge_files/May24_dl90_2ndEnd.csv")  # May24 2nd end (notated on chain itself)
-            object.__setattr__(self, "thresh", 1.15)  # May24 2nd end (notated on chain itself)
-            # object.__setattr__(self, "tau_file", "single_hinge_files/May24_dl90_1stEnd.csv")   # May24 1st end (notated on chain itself)
-            # object.__setattr__(self, "thresh", 0.96)  # May24 1st end (notated on chain itself)
+            # object.__setattr__(self, "tau_file", "single_hinge_files/May24_dl90_2ndEnd.csv")  # May24 2nd end (notated on chain itself)
+            # object.__setattr__(self, "thresh", 1.15)  # May24 2nd end (notated on chain itself)
+            object.__setattr__(self, "tau_file", "single_hinge_files/May24_dl90_1stEnd.csv")   # May24 1st end (notated on chain itself)
+            object.__setattr__(self, "thresh", 0.96)  # May24 1st end (notated on chain itself)
             # object.__setattr__(self, "thresh", 1.99)  # Feb23
             # object.__setattr__(self, "thresh", 1.58)
             # object.__setattr__(self, "thresh", 1.9)  # Feb22 measurements from just before Red South
@@ -182,7 +182,7 @@ class TrainingConfig:
     # dataset_sampling = 'stress strain'
 
     # dataset_file: str = r"Predetermined trajectory\Mar23\buckle={}.csv"
-    dataset_file: str = r"Predetermined trajectory\May27\short_arc\May24Chain_2ndEnd_stifferk\buckle={}.csv"
+    dataset_file: str = r"Predetermined trajectory\May27\short_arc\May24Chain_1stEnd\buckle={}.csv"
     # dataset_file: str = r"Predetermined trajectory\Mar30\zeroDeg_farther\buckle={}.csv"
 
     # # tip values to buckle shims - 'BEASTAL' for the BEASTAL scheme, else 'one_to_one'
